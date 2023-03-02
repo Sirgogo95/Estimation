6272bca396ddf4a79f5c1e7612652f7c20002f0a
Sirgogo95 <sergecelestint@gmail.com>
2023-03-02 09:11:33 -0400

    modified:   Material_Analisis.py

--- Material_Analisis.py
+++ Material_Analisis.py
@@ -1,10 +1,22 @@
 from Material import Material
 
 class Material_Analisis(Material):
-    def __init__(self, codigo, familias = set() , nombre = "" , alias = set() , precio = 0.0 , tasa = 0.0 , unidad = "U", itbis = 0.0, manejo = 0.0):
+    def __init__(self, codigo, familias = set() , nombre = "" , alias = set() , precio = 0.0 , tasa = 0.0 , unidad = "U", cantidad = 0.0, itbis = 0.0, manejo = 0.0):
         super().__init__(codigo ,familias, nombre, alias, precio, tasa, unidad)
+        self.cantidad = cantidad
         self.itbis = itbis
         self.manejo = manejo
+
+    @property
+    def cantidad(self):
+        return self._cantidad
+    
+    @cantidad.setter
+    def cantidad(self, cantidad = 0.0):
+        if cantidad < 0:
+            raise ValueError("Cantidad no puede ser negativa")
+        else:
+            self._cantidad = cantidad
 
     @property
     def itbis(self):
@@ -22,8 +34,44 @@ class Material_Analisis(Material):
     def manejo(self, manejo = 0.0):
         self._manejo = manejo/100
 
+    @property
+    def itbism(self):
+        return self._itbis * self.precio
+
+    @property
+    def itbism_total(self):
+        return self._itbis * self.precio * self.cantidad
+
+    @property
+    def manejom(self):
+        return self._manejo * self.precio
+
+    @property
+    def manejom_total(self):
+        return self._manejo * self.precio * self.cantidad
+
+    @property
+    def precio_itbis(self):
+        return self.precio * (1 + self.itbis)
+
+    @property
+    def precio_total(self):
+        return self.precio * self.cantidad 
+
+    @property
+    def precio_total_itbis(self):
+        return self.precio * self.cantidad * (1 + self.itbis)
+    
+    @property
+    def precio_total_itbis_manejo(self):
+        return self.precio * self.cantidad * (1 + self.itbis) * (1 + self.manejo)
+    
+
+def main():
+    m = Material_Analisis("HN",set("Hierro negro"),"Tubo de Hierro negro","",150,30,"U",5,18,10)
+    n = Material_Analisis("HN",set("Hierro negro"),"Tubo de Hierro negro","",200,10,"U",3,18,10)
+    print(n)
 
 
-m = Material_Analisis("HN",set("Hierro negro"),"Tubo de Hierro negro","",150,30,"U",18,10)
-n = Material_Analisis("HN",set("Hierro negro"),"Tubo de Hierro negro","",200,10,"U",18,10)
-print(m.itbis)
+if __name__ == "__main__":
+    main()
