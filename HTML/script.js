a37db19bb83fa2b948ce46dbe09a855d3d7151b4
Sirgogo95 <sergecelestint@gmail.com>
2023-03-05 16:14:36 -0400

    modified:   HTML/Style.css 	modified:   HTML/Test.html 	new file:   HTML/script.js

---
+++ HTML/script.js
@@ -0,0 +1,16 @@
+let options = document.querySelector(".options");
+let menu_options = document.querySelector(".menu_options");
+let logo_name = document.querySelector(".logo_name");
+let menu_button = document.querySelector(".menu_button");
+let sidebar = document.querySelector(".sidebar");
+let wrapper = document.querySelector(".wrapper");
+
+
+function inactivate(){
+    options.classList.toggle("inactive-options");
+    menu_options.classList.toggle("inactive-menu_options");
+    logo_name.classList.toggle("inactive-logo_name");
+    sidebar.classList.toggle("inactive-sidebar");
+    wrapper.classList.toggle("inactive-wrapper");
+
+}
