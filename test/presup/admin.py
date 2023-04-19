66f94f7b2ce693c0fd9bb5d102fcf9e168cfc5ff
Sirgogo95 <sergecelestint@gmail.com>
2023-04-19 15:03:01 -0400

    modified:   test/db.sqlite3 	modified:   test/presup/admin.py

--- test/presup/admin.py
+++ test/presup/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Material, Suplidor, Material_Analisis
+from .models import Material, Suplidor, Material_Analisis, Proyecto, Cliente
 
 
 # Register your models here.
 admin.site.register(Material)
 admin.site.register(Suplidor)
 admin.site.register(Material_Analisis)
+admin.site.register(Cliente)
+admin.site.register(Proyecto)
