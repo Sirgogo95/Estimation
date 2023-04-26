279c03898e96bbf6a0627011a260d074ed9a2aaf
Sirgogo95 <sergecelestint@gmail.com>
2023-04-26 01:04:23 -0400

    modified:   test/presup/templates/presup/layout.html

--- test/presup/static/presup/modalScript.js
+++ test/presup/static/presup/modalScript.js
@@ -1,14 +1,13 @@
 ; (function () {
-    const modal = document.getElementById('modal')
+    
 
     htmx.on('htmx:afterSwap', (e) => {
-        const modalToggle = document.getElementById('modal'); modalToggle.show(modal)
+        const modal = new bootstrap.Modal(document.getElementById('modal')) 
+        console.log('htmx:afterSwap', e)
+        if(e.detail.target.id === "modal_container")
+            modal.modal('show')
     })
 
-    htmx.on('htmx:beforeSwap', (e) => {
-        console.log('htmx:beforeSwap', e)
-            if (e.detail.target.id === "modal_container" && !e.detail.xhr.response) 
-                modal.hide()
-    })
+
 }
     )()
